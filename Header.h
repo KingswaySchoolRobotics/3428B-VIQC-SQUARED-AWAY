f4b6452d589e44bbd03eaba1dbe978afd1990434
Joseph Greening <45278068+YoloPigeon@users.noreply.github.com>
2019-05-04 06:30:35 +1200

    Edit 2

--- Header.h
+++ Header.h
@@ -1,14 +1,3 @@
-#pragma config(Motor,  motor1,          Left,          tmotorVexIQ, PIDControl,encoder)
-#pragma config(Motor,  motor2,         ArmRight,      tmotorVexIQ, PIDControl, reversed, encoder)
-#pragma config(Motor,  motor3,         ArmLeft,       tmotorVexIQ, PIDControl, encoder)
-#pragma config(Motor,  motor4,         Intake,      tmotorVexIQ, PIDControl, reversed, encoder)
-#pragma config(Motor,  motor5,         CubeClaw,       tmotorVexIQ, PIDControl, encoder)
-#pragma config(Motor,  motor6,          Right,         tmotorVexIQ, PIDControl, reversed, encoder)
-#pragma config(Sensor, port7,  Main_Gyro,      sensorVexIQ_Gyro)
-#pragma config(Sensor, port8,  ArmBottomBumper, sensorVexIQ_Touch)
-#pragma config(Sensor, port9,  ArmTopBumper,   sensorVexIQ_Touch)
-#pragma config(Sensor, port10,  LED,            sensorVexIQ_LED)
-#pragma config(Sensor, port7,  CenterColor,      sensorVexIQ_ColorGrayscale)
-#pragma config(Sensor, port8,  CrossColor,       sensorVexIQ_ColorGrayscale)
+
 
 //comment out bottom 2 lines for Greening's Testing robot.
